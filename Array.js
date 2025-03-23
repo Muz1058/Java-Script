01a17a41ee1257ebfaaa6d6630994ab1b0240578
Muz1058 <muzaffarali3633@gmail.com>
2025-03-23 16:24:41 +0500

    Create Array.js
    Given an integer array arr and a filtering function fn, return a filtered array filteredArr.
    
    The fn function takes one or two arguments:
    
    arr[i] - number from the arr
    i - index of arr[i]
    filteredArr should only contain the elements from the arr for which the expression fn(arr[i], i) evaluates to a truthy value. A truthy value is a value where Boolean(value) returns true.
    
    Please solve it without the built-in Array.filter method

---
+++ Array.js
@@ -0,0 +1,24 @@
+//Filter Elements from Array
+
+var filter = function(arr, fn) {
+    let filteredArray=[],count=0;
+    for(let i=0;i<arr.length;i++){
+        if(fn(arr[i],i)){
+            filteredArray[count++]=arr[i];
+        }
+    }
+    return filteredArray;
+};
+
+const fn=function(n){return n>10};
+function firstIndex(n, i) { return i === 0; };
+
+let arr=[10,20,30];
+console.log(filter(arr,fn))
+
+let arr1=[1,2,3];
+console.log(filter(arr1,firstIndex))
+
+const arr3 = [-2, -1, 0, 1, 2];
+const plusOne = function (n) { return n + 1; };
+console.log(filter(arr3, plusOne)); 
