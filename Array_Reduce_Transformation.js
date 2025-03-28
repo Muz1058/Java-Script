ae84642df12361823e28d069f84d5aee4e6b9be0
Muz1058 <muzaffarali3633@gmail.com>
2025-03-28 22:18:03 +0500

    Create Array_Reduce_Transformation.js

---
+++ Array_Reduce_Transformation.js
@@ -0,0 +1,12 @@
+
+
+var reduce = function(nums, fn, init) {
+    let result=init;
+    for(let i=0;i<nums.length;i++){
+        result=fn(result,nums[i]);
+    }
+    return result;
+
+
+    return nums.reduce(fn,init);
+};
