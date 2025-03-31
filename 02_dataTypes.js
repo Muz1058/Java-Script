5cb4f438dd51647f02326d9980954838bb305ea5
Muz1058 <muzaffarali3633@gmail.com>
2025-03-31 17:02:54 +0500

    Create 02_dataTypes.js

---
+++ 02_dataTypes.js
@@ -0,0 +1,16 @@
+"strict use";   // treat complete code as new java script
+
+//alert(3+3);  // error , as we are using node.js not a browser
+
+// number     ==> 2^53
+//bigint      ==>
+// String     ==> single and double quote
+//boolean     ==> true/false
+//null        ==> stand alone value
+//undefined   ==> data type not specified
+//symol       ==> unique
+//object      ==>(null is also an object)
+
+
+//typeof      ==> use to find Data Type
+
