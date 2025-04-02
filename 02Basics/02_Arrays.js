4d227c638567f468291440854644bcefef61ef03
Muz1058 <muzaffarali3633@gmail.com>
2025-04-02 21:44:26 +0500

    Create 02_Arrays.js

---
+++ 02Basics/02_Arrays.js
@@ -0,0 +1,10 @@
+const marvelHeros=["thor","Iron man","SpiderMan"]
+const dc=["Super MAn","Flash","Batman"]
+
+marvelHeros.push(dc);  //in JS array can store data with different data types even arrays,
+//                     //in this case marvelHeros will store dc as third element of array
+console.log(marvelHeros[3][0]);  // we can access eash element of pushed array by marvelHeros[3][1] 
+                           
+console.log(marvelHeros);
+
+
