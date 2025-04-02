84fbaeaeb214beaca4ae28b033e3c2507959bb04
Muz1058 <muzaffarali3633@gmail.com>
2025-04-02 22:03:04 +0500

    Update 02_Arrays.js

--- 02Basics/02_Arrays.js
+++ 02Basics/02_Arrays.js
@@ -7,30 +7,44 @@
                            
 // console.log(marvelHeros);
 
-//--------------Array Concatination---------------------------
-//    .concat method merge two existing arrays and will produce new resultant array
+// //--------------Array Concatination---------------------------
+// //    .concat method merge two existing arrays and will produce new resultant array
  
-const concatenatedHero= marvelHeros.concat(dc);
+// const concatenatedHero= marvelHeros.concat(dc);
 
-//console.log(concatenatedHero);
+// //console.log(concatenatedHero);
 
-//-----------------Spread operator-----------------------
+// //-----------------Spread operator-----------------------
 
-const spreadHeros =[...marvelHeros,...dc];
-//console.log(spreadHeros);
+// const spreadHeros =[...marvelHeros,...dc];
+// //console.log(spreadHeros);
 
-//----------------flat method------------------
-/*The maximum recursion depth
-Returns a new array with all sub-array elements concatenated into it recursively up to the specified depth. */
-const anotherAyyay=[1,2,3,[4,5,6],7,[6,7,[4,5]]];
-console.log(anotherAyyay);   //[ 1, 2, 3, [ 4, 5, 6 ], 7, [ 6, 7, [ 4, 5 ] ] ]
-                            // Array in array inside another Array
-const real_another_array=anotherAyyay.flat(Infinity)
+// //----------------flat method------------------
+// /*The maximum recursion depth
+// Returns a new array with all sub-array elements concatenated into it recursively up to the specified depth. */
+// const anotherAyyay=[1,2,3,[4,5,6],7,[6,7,[4,5]]];
+// console.log(anotherAyyay);   //[ 1, 2, 3, [ 4, 5, 6 ], 7, [ 6, 7, [ 4, 5 ] ] ]
+//                             // Array in array inside another Array
+// const real_another_array=anotherAyyay.flat(Infinity)
 
-console.log(real_another_array);
-//output 
-/*[
-    1, 2, 3, 4, 5,
-    6, 7, 6, 7, 4,
-    5
-  ]*/
+// console.log(real_another_array);
+// //output 
+// /*[
+//     1, 2, 3, 4, 5,
+//     6, 7, 6, 7, 4,
+//     5
+//   ]*/
+
+//------------------To Array Conversion--------------------------
+console.log(Array.isArray("Abs"));   //false because "abs"  is String
+console.log(Array.from("Abs"));      //will convert string to Array
+
+console.log(Array.from({name: "xyz"}));  //#intresting 
+
+
+//-------------------of Array method---------------------
+const score1=100
+const score2=2100
+const score3=3100
+
+console.log(Array.of(score1,score2,score3));  //will  produce array of score1,2,3
