b9f34a47cabc3b472de5b044c77377f62a5dd559
Muz1058 <muzaffarali3633@gmail.com>
2025-04-02 21:48:42 +0500

    Update 02_Arrays.js

--- 02Basics/02_Arrays.js
+++ 02Basics/02_Arrays.js
@@ -1,10 +1,20 @@
-const marvelHeros=["thor","Iron man","SpiderMan"]
-const dc=["Super MAn","Flash","Batman"]
+ const marvelHeros=["thor","Iron man","SpiderMan"]
+ const dc=["Super MAn","Flash","Batman"]
 
-marvelHeros.push(dc);  //in JS array can store data with different data types even arrays,
-//                     //in this case marvelHeros will store dc as third element of array
-console.log(marvelHeros[3][0]);  // we can access eash element of pushed array by marvelHeros[3][1] 
+// marvelHeros.push(dc);  //in JS array can store data with different data types even arrays,
+// //                     //in this case marvelHeros will store dc as third element of array
+// console.log(marvelHeros[3][0]);  // we can access eash element of pushed array by marvelHeros[3][1] 
                            
-console.log(marvelHeros);
+// console.log(marvelHeros);
 
+//--------------Array Concatination---------------------------
+//    .concat method merge two existing arrays and will produce new resultant array
+ 
+const concatenatedHero= marvelHeros.concat(dc);
 
+//console.log(concatenatedHero);
+
+//-----------------Spread operator-----------------------
+
+const spreadHeros =[...marvelHeros,...dc]
+console.log(spreadHeros);
