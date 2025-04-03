bea8a44b80579f4e6717180881a9f71fd8305547
Muz1058 <muzaffarali3633@gmail.com>
2025-04-03 23:44:13 +0500

    Update one.js

--- 04ControlFlow/one.js
+++ 04ControlFlow/one.js
@@ -11,11 +11,11 @@ if ( temperature === 40 ){
 console.log("Execute");
 // <, >, <=, >=, ==, !=, ===, !==
 
-// const score = 200
+const score = 200
 
-// if (score > 100) {
-//     let power = "fly"
-//     console.log(`User power: ${power}`);
-// }
+if (score > 100) {
+    let power = "fly"
+    console.log(`User power: ${power}`);
+}
 
 // console.log(`User power: ${power}`);
