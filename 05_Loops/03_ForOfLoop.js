cac368d1c6e8e8ed91166bb012dec874242bb54d
Muz1058 <muzaffarali3633@gmail.com>
2025-04-05 21:33:44 +0500

    Update 03_ForOfLoop.js

--- 05_Loops/03_ForOfLoop.js
+++ 05_Loops/03_ForOfLoop.js
@@ -5,7 +5,7 @@
 
 const arr=[1,2,3,4,5]
 for (const num of arr) {
-    console.log(`value = ${num}`);
+   // console.log(`value = ${num}`);
     
 }
 
@@ -14,7 +14,7 @@ const greet="Hello world"
 
 for (const element of greet) {
     
-    console.log(`${element}`);
+  //  console.log(`${element}`);
     
 }
 
@@ -23,4 +23,22 @@ for (const element of greet) {
 
 
 const map=new Map()
-map.
+map.set("pk","Pakistan")
+map.set("KSA","Saudi Arabia")
+map.set("UAE","United Arab Emirates")
+
+for (const [key,value] of map) {
+   // console.log(key);    
+   // console.log(value);
+   //console.log(key,"->",value);
+}
+//console.table(map);
+
+const myObj={
+    'g1':'NFC',
+    'g2':'Spiderman'
+}
+
+// for (const key of myObj) {  //TypeError: myObj is not iterable
+//     console.log(key);
+// }
