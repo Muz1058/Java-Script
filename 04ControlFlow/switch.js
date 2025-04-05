559913ab95624ce0acee89e7fc3fcc62ae1c93dc
Muz1058 <muzaffarali3633@gmail.com>
2025-04-05 15:26:52 +0500

    Create switch.js

---
+++ 04ControlFlow/switch.js
@@ -0,0 +1,27 @@
+
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month=2
+switch (month) {
+    case 1:
+        console.log("jan")
+        break;
+    case 2:
+        console.log("Feb")
+        //break;
+    case 3:
+        console.log("March")
+        break;
+
+    default:
+        console.log("Default")
+        break;
+}
