28bb7110d067e2a46fce6e5d2738dca17016810a
Muz1058 <muzaffarali3633@gmail.com>
2025-04-05 20:05:39 +0500

    Create 03_ForOfLoop.js

---
+++ 05_Loops/03_ForOfLoop.js
@@ -0,0 +1,26 @@
+//For of
+
+// ["","",""]
+// [{},{},{}]
+
+const arr=[1,2,3,4,5]
+for (const num of arr) {
+    console.log(`value = ${num}`);
+    
+}
+
+
+const greet="Hello world"
+
+for (const element of greet) {
+    
+    console.log(`${element}`);
+    
+}
+
+
+//Maps
+
+
+const map=new Map()
+map.
