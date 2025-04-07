196a14f8972204d2926af0b8b1e53b45c1df30f6
Muz1058 <muzaffarali3633@gmail.com>
2025-04-07 23:01:17 +0500

    Create map.js

---
+++ 05_Loops/map.js
@@ -0,0 +1,17 @@
+const myNumers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+//The map() method of Array instances creates a new array populated with the results of calling a provided function on every element in the calling array.
+ //const newNums = myNumers.map( (num) => { return num + 10})
+ 
+ //console.log(newNums);
+
+
+
+const newNums = myNumers
+                .map((num) => num * 10 )
+                .map( (num) => num + 1)
+                .filter( (num) => num >= 40)
+
+console.log(newNums); //[ 41, 51,  61, 71,81, 91, 101 ]
+
