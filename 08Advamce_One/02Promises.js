de2adff7165fb25839c9e7288803f0336869bdbe
Muz1058 <muzaffarali3633@gmail.com>
2025-04-15 21:19:34 +0500

    Update 02Promises.js

--- 08Advamce_One/02Promises.js
+++ 08Advamce_One/02Promises.js
@@ -2,3 +2,25 @@
 
 */
 
+
+//creation
+const promiseOne= new Promise(function(resolve,reject){
+    //Do an async task
+    //DB Calls, cryptoGraphy, network
+
+    setTimeout(function(){
+        console.log("async task is completed");
+        resolve()
+    },1000)
+});
+
+//CONSUPTION
+
+
+// resolve connection .then()
+
+promiseOne.then(function(){
+    console.log("Promise consumed");
+})
+
+
