d299fd6cecdc78e8804a02304d6b3b66302a3d43
Muz1058 <muzaffarali3633@gmail.com>
2025-04-15 22:34:22 +0500

    Update 02Promises.js

--- 08Advamce_One/02Promises.js
+++ 08Advamce_One/02Promises.js
@@ -110,3 +110,17 @@ consumePromiseFive()
 // }).finally(()=>{
 
 // })
+
+
+async function getAllUser() {
+ try{
+    const response=await  fetch("https://jsonplaceholder.typicode.com/users")
+
+    const data=await response.json()
+    console.log(data);
+ }
+ catch(error){
+console.log("E::",error);
+ }
+}
+getAllUser()
