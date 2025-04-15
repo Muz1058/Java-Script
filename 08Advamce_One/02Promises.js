f66163375920b45dc26ba998ee0b631bb15e1206
Muz1058 <muzaffarali3633@gmail.com>
2025-04-15 21:52:39 +0500

    Update 02Promises.js

--- 08Advamce_One/02Promises.js
+++ 08Advamce_One/02Promises.js
@@ -24,3 +24,15 @@ promiseOne.then(function(){
 })
 
 
+
+new Promise(function(resolve,reject){
+    setTimeout(function(){
+        console.log("Async task 2");
+        resolve();
+    },1000)
+}).then(function(){
+    console.log("Async 2 Resolve");
+})
+
+
+
