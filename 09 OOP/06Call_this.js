b6a6ba77d6c7b783b3e10b3b8d903f8fb4fec290
Muz1058 <muzaffarali3633@gmail.com>
2025-04-18 23:43:50 +0500

    Create 06Call_this.js

---
+++ 09 OOP/06Call_this.js
@@ -0,0 +1,34 @@
+//Call
+
+
+function SetUsername(username){
+    //complex DB calls
+    this.username = username
+    console.log("called");
+}
+
+// function createUser(username, email, password){
+//     SetUsername( username)
+   
+//     this.email = email
+//     this.password = password
+// }
+
+
+
+// function createUser(username, email, password){
+//     SetUsername.call(username)
+   
+//     this.email = email
+//     this.password = password
+// }
+
+function createUser(username, email, password){
+    SetUsername.call(this, username)
+   
+    this.email = email
+    this.password = password
+}
+
+const chai = new createUser("chai", "chai@fb.com", "123")
+console.log(chai);
